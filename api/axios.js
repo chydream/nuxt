33aa158bf863821fb14a5593ac8e7123fe44e66c
unknown <710962261@qq.com>
2020-09-02 10:09:51 +0800

    细节修改

--- api/axios.js
+++ api/axios.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
-import store from '@/store'
-import { Message } from 'element-ui'
+// import {state} from '@/store/user'
+// import { Message } from 'element-ui'
 
 axios.defaults.timeout = 20000
 axios.defaults.withCredentials = true // 跨域请求，允许保存cookie
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
-  console.log(store().getters.token)
   if (process.client && $nuxt.$store.getters.token  && sessionStorage.getItem('token')) {
     config.headers['Authorization'] = $nuxt.$store.getters.token
   }
